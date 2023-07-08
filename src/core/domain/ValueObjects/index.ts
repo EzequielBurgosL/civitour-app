567710525c8c96cb0a45b1eed121de7b2dcd0ad5
EzequielBurgosL <ezbulo@gmail.com>
2023-07-08 11:05:14 +0200

    fix: remove unnecessary comment on ValueObjects

--- src/core/domain/ValueObjects/index.ts
+++ src/core/domain/ValueObjects/index.ts
@@ -15,11 +15,6 @@ interface ValueObjectProps {
   [index: string]: any;
 }
 
-/**
- * @desc ValueObjects are objects that we determine their
- * equality through their structrual property.
- */
-
 export abstract class ValueObject<T extends ValueObjectProps> {
   public readonly props: T;
 
