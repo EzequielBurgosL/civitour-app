8b4085fb830b9c8ef68196ecc4347eaf1f2d359a
EzequielBurgosL <ezbulo@gmail.com>
2023-07-08 10:46:50 +0200

    chore: add project dependencies, audit and sonar files

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['xo'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['**/dist/**/*.js'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['Error', 2],
+    'object-curly-spacing': ['Error', 'always'],
+    'operator-linebreak': ['error', 'after'],
+    'no-unused-vars': 'off',
+    'generator-star-spacing': ['error', { before: false, after: true }],
+    'no-useless-constructor': 'off',
+  },
+};
